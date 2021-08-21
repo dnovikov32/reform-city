baa1c716cb734e4c9e5ddb590222261b811ff334
Дмитрий Новиков <dn@t1000.su>
2021-08-21 16:43:29 +0300

    Feat(common): Refactor directory structure

--- src/Presentation/Web/Site/Controller/FrontController.php
+++ src/Presentation/Web/Site/Controller/FrontController.php
@@ -4,13 +4,29 @@ declare(strict_types=1);
 
 namespace App\Presentation\Web\Site\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-class FrontController
+class FrontController extends AbstractController
 {
+    private HttpKernelInterface $httpKernel;
+
+    /**
+     * @param HttpKernelInterface $httpKernel
+     */
+    public function __construct(HttpKernelInterface $httpKernel)
+    {
+        $this->httpKernel = $httpKernel;
+    }
+
     public function __invoke(Request $request)
     {
         dd('ololo', $request);
+//        $url = ($getUrlBySlug)($request->attributes->get('slug'));
+
+        $this->httpKernel->handle('test');
+
     }
 
 }
